2e26f5280ea53ea10ac18c6d3308f07dc404342b
devang27 <ec_devang@yahoo.com>
2017-03-07 00:27:02 -0500

    Add files via upload

---
+++ test/e2e/personPageData.js
@@ -0,0 +1,11 @@
+module.exports = {
+	data: { 
+		name : 'test',
+		height: '5.10',
+		mass:'mass',
+ 		hairColor :'black',
+ 		eyeColor :'Brown',
+ 		birthYear :'1983',
+ 		gender :'Male'
+}
+};
---
+++ test/e2e/planetPageData.js
@@ -0,0 +1,14 @@
+module.exports = {
+	data: { 
+		nameText : 'test',
+		rotationPeriod: 'Monday',
+		orbitalPeriod:'Orbital',
+ 		diameter :'15.5',
+ 		climate :'40f',
+ 		gravity :'100',
+ 		terrain :'101',
+ 		surfaceWater :'102',
+ 		population :'103'
+}
+};
+
